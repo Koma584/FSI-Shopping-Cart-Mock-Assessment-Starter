a3d36c8a1080c873380fe3c4c88820304a14b676
Kirklund Brown <kirklundbrown@gmail.com>
2022-08-02 19:53:16 -0700

    update number

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//function to change disaplay
+function updateQuantity(displayQuantity) {
+  let num = document.querySelector(".total-quantity");
+  quantity.innerHTML = displayQuantity;
+}
+let add = document.getElementById("quantity-up");
+//add button function
+add.addEventListener("click", function () {
+  num++;
+  updateQuantity(`Quantity: ${quantity}`);
+});
+
+let sub = document.getElementById("quantity-down");
+//add button function
+sub.addEventListener("click", function () {
+  //num not named
+  if (num > 0) {
+    num--;
+  }
+  updateQuantity(`Quantity: ${num}`);
+});
